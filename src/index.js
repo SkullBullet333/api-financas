fc86b04be634381e1b862f911574d3d8f745b2e8
SkullBullet333 <rrochapablo@gmail.com>
2026-03-13 17:35:07 -0300

    Configura Prisma, banco de dados PostgreSQL e rotas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ const { PrismaClient } = require('@prisma/client');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
-const prisma = new PrismaClient();
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
